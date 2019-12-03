e1cc577faa22fdbb206e9e109498575558bde070
fawwazmayda <maydafawwaz@gmail.com>
2019-12-03 12:54:37 +0700

    python manage.py makemigrations budget

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Budget(models.Model):
+    nama = models.CharField(max_length=200)
+    harga = models.IntegerField()
+    satuan = models.IntegerField()
+
+class Warga(models.Model):
+    username = models.CharField(max_length=200)
+    komentar = models.CharField(max_length=800)
