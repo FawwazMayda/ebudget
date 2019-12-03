c5e6dc61bc19751b3f564dade83c1385ba97abeb
fawwazmayda <maydafawwaz@gmail.com>
2019-12-03 13:09:08 +0700

    Fix __str__ error

--- budget/models.py
+++ budget/models.py
@@ -6,6 +6,12 @@ class Budget(models.Model):
     harga = models.IntegerField()
     satuan = models.IntegerField()
 
+    def __str__(self):
+        return "{nama} {harga} {satuan}".format(nama=self.nama,harga=self.harga,satuan=self.satuan)
+
 class Warga(models.Model):
     username = models.CharField(max_length=200)
     komentar = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.komentar
