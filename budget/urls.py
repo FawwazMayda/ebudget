89cf29299f93bf10a834126c850745fd3b89b8ec
fawwazmayda <maydafawwaz@gmail.com>
2019-12-03 14:05:47 +0700

    Budget adding is OK

--- budget/urls.py
+++ budget/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.halaman_budget,name='index'),
+    path('budget_detail/<int:budget_id>',views.detail_budget,name='detail_budget')
+
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Budget
 
 # Create your views here.
-def index(request):
-    print("ABANG OH BOLA")
+def halaman_budget(request):
     return HttpResponse("Ini Budget KU")
+
+def detail_budget(request,budget_id):
+    budget_list = Budget.objects.all()
+    print("HEYY SINIII")
+    print(budget_list)
+    template = loader.get_template("budget_detail.html")
+    context = {
+        'Budgets' : budget_list
+    }
+    return HttpResponse(template.render(context,request))
