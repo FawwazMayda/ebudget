42c5e463e2a60feda7dcb3ef8a1121c906b55ef2
fawwazmayda <maydafawwaz@gmail.com>
2019-12-05 21:53:59 +0700

    otp works

--- coba.py
+++ coba.py
@@ -1,8 +1,15 @@
-import request 
-'https://api.thebigbox.id/sms-otp/1.0.0/otp/96/' PUT
-'https://api.thebigbox.id/sms-otp/1.0.0/otp/96/verifications' POSt
+import requests
+import json
+url0 = 'https://api.thebigbox.id/sms-otp/1.0.0/otp/32/' #PUT
+url1 = 'https://api.thebigbox.id/sms-otp/1.0.0/otp/32/verifications' #POST
+api_key = "3tqkUH1dNdQznlqCDO3hOWvvvddihjBZ"
+header = {
+  'Content-type':"application/json",
+  'x-api-key':api_key,
+  'Accept':'application/json'
+}
 
-{
+body0 = {
   "maxAttempt": 2,
   "masking": {
     "accountId": "",
@@ -16,9 +23,23 @@ import request
   "callbackUrl": ""
 }
 
-{
+body1 = {
   "maxAttempt": 2,
   "otpstr": "6000",
   "expireIn": 3600,
   "digit": 4
 }
+
+def makeOTP(key):
+  url = 'https://api.thebigbox.id/sms-otp/1.0.0/otp/{key}'.format(key=key)
+  return requests.put(url,headers=header,data=json.dumps(body0))
+
+def verifyOTP(key,kode):
+  url = 'https://api.thebigbox.id/sms-otp/1.0.0/otp/{key}/verifications'.format(key=key)
+  bodyC = {
+  "maxAttempt": 2,
+  "otpstr": kode,
+  "expireIn": 3600,
+  "digit": 4
+  }
+  return requests.post(url,headers=header,data=json.dumps(bodyC))
